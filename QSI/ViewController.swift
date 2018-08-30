190ee96d82e99f2b289547217e897dc5350b26ca
Siva Dayalan <sdayalan@gmail.com>
2018-08-30 20:08:55 +0800

    Initial Commit

---
+++ QSI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QSI
+//
+//  Created by Siva Dayalan on 30/8/18.
+//  Copyright © 2018 Siva Dayalan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
